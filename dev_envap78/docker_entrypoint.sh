75db8e23bbefdbcf68900951aacb8c23fbccb902
장성현 <pig492@gmail.com>
2024-12-15 14:38:28 +0900

    버그 수정 및 PHP 8.1 차가가

--- dev_envap78/docker_entrypoint.sh
+++ dev_envap78/docker_entrypoint.sh
@@ -2,4 +2,10 @@
 
 /usr/bin/sphp "$PHP"
 /usr/bin/svhost "$CONF" "$DOCUMENT_ROOT"
+if [ -f "/usr/lib/php/custom/${CUSTOM_INI}" ]; then
+    ln -s "/usr/lib/php/custom/${CUSTOM_INI}" "/etc/php/${PHP}/apache2/conf.d/35-${CUSTOM_INI}"
+    ln -S "/usr/lib/php/custom/${CUSTOM_INI}" "/etc/php/${PHP}/apache2/conf.d/35-${CUSTOM_INI}"
+    ln -s "/usr/lib/php/custom/${CUSTOM_INI}" "/etc/php/${PHP}/cli/conf.d/35-${CUSTOM_INI}"
+    ln -S "/usr/lib/php/custom/${CUSTOM_INI}" "/etc/php/${PHP}/cli/conf.d/35-${CUSTOM_INI}"
+fi
 exec "$@"
--- dev_envap78/sphp.sh
+++ dev_envap78/sphp.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ "$1" == "7.4" ]; then
-    a2dismod "php$current"
     a2enmod php7.4
     update-alternatives --set php /usr/bin/php7.4
+elif [ "$1" == "8.1" ]; then
+    a2enmod php8.1
+    update-alternatives --set php /usr/bin/php8.1
 elif [ "$1" == "8.2" ]; then
-    a2dismod "php$current"
     a2enmod php8.2
     update-alternatives --set php /usr/bin/php8.2
 elif [ "$1" == "8.4" ]; then
-    a2dismod "php$current"
     a2enmod php8.4
     update-alternatives --set php /usr/bin/php8.4
 fi
