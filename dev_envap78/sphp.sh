7065836c2d967ed191ea480bac0a1ef036dd9d9d
장성현 <pig492@gmail.com>
2025-03-04 22:05:06 +0900

    8.0 추가가

--- dev_envap78/sphp.sh
+++ dev_envap78/sphp.sh
@@ -3,6 +3,9 @@
 if [ "$1" == "7.4" ]; then
     a2enmod php7.4
     update-alternatives --set php /usr/bin/php7.4
+elif [ "$1" == "8.0" ]; then
+    a2enmod php8.0
+    update-alternatives --set php /usr/bin/php8.0
 elif [ "$1" == "8.1" ]; then
     a2enmod php8.1
     update-alternatives --set php /usr/bin/php8.1
